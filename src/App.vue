66ed546bf935a9c9ba5ffc506655f4ed4df0dee4
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-15 14:17:33 +0100

    BACKUP

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 export default {
   name: "App",
+  components: { AppHeader, AppMain, AppFooter },
 };
 </script>
 
-<template></template>
+<template>
+  <app-header />
+  <app-main />
+  <app-footer />
+</template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use './style/general'
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,95 @@
 <script>
 export default {
     name: "AppHeader",
+    data(){
+        return{
+            navMenu:[
+                {
+                    text: 'Characters',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'Comics',
+                    href: '#',
+                    active: true
+                },
+                {
+                    text: 'Movies',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'Tv',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'Games',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'ollectibles',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'Videos',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'Fans',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'News',
+                    href: '#',
+                    active: false
+                },
+                {
+                    text: 'Shop',
+                    href: '#',
+                    active: false
+                },
+            ]
+        }
+    }
 };
 </script>
 
-<template></template>
+<template>
 
-<style lang="scss" scoped></style>
+<header>
+    
+    <div class="container">
+
+        <div class="logo">
+
+            <img :src="`/dc-logo.png`" alt="">
+
+        </div>
+
+        <nav>
+            <ul>
+                <li
+                    v-for="(link, index) in navMenu"
+                    :key="index">
+                <a href="#">${link.text}</a></li>
+            </ul>
+        </nav>
+
+    </div>
+
+</header>
+
+</template>
+
+<style lang="scss" scoped>
+    header{
+        background-color: #fff;
+        padding: 20px 0;
+    }
+</style>
