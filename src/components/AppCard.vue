66265277fd55f1502f31b530bae6d4fe0b691db8
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-16 14:01:59 +0100

    [add] AppCard

---
+++ src/components/AppCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: 'AppCard',
+    props: {
+        cardItem : String
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-image">
+            <img :src="cardItem.thumb" alt="cardItem.series">
+        </div>
+        <div class="card-text">
+            <span>{{cardItem.series}}</span>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss">
+    @use '../style/partials/variables' as *;
+
+</style>
