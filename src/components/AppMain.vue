75753563e5929dff5a53c6e37cf9f259b099ce42
MatTheoo89 <matteobellomo89@gmail.com>
2022-11-16 13:35:08 +0100

    [add] data + edit Main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,10 +8,19 @@ export default {
 
     <main>
 
+        <div class="jumbotron">
+            <img src="../assets/img/jumbotron.jpg" alt="jumbotron">
+        </div>
+
         <div class="container">
 
-        <h1> ==| Content goes here |== </h1>
+            <button class="top">Current Series</button>
+            
+            <div class="card-area">
 
+                <button>Load More</button>
+            </div>
+            
         </div>
 
     </main>
@@ -21,6 +30,38 @@ export default {
 <style lang="scss" scoped>
 
     @use '../style/partials/variables' as *;
+
+    .jumbotron{
+        background-color: red;
+        height: 500px;
+        object-fit: cover;
+        object-position: center;
+        overflow: hidden;
+        img{
+            width: 100%;
+        }
+    }
+
+    button{
+        border: none;
+        outline: none;
+        background-color: $blue-color;
+        padding: 10px 25px;
+        color: #fff;
+        font-size: 1.2rem;
+        text-transform: uppercase;
+        text-align: center;
+        &.top{
+            font-weight: 500;
+            transform: translateY(-230%);
+        }
+    }
+
+    .card-area{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 
     .container{
         color: #fff;
